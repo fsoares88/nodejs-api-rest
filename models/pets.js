21e22bf4a2594933258c323a3e8ef3a8d7c1af8f
Fabio Alves Soares <fabiocorinthiano88@gmail.com>
2021-05-18 09:13:26 -0300

    inclusão do modulo de pets

---
+++ models/pets.js
@@ -0,0 +1,26 @@
+const conexao = require('../infraestrutura/conexao')
+const uploadDeArquivo = require('../arquivos/uploadDeArquivos')
+
+class Pet {
+    adiciona(pet, res) {
+        const sql = 'INSERT INTO pets SET ?'
+
+        uploadDeArquivo(pet.imagem, pet.nome, (erro, novoCaminho) => {
+            if(erro) {
+                res.status(400).json({ erro })
+            } else {
+                const novoPet = {nome: pet.nome, imagem: novoCaminho}
+
+                conexao.query(sql, novoPet, erro => {
+                    if(erro) {
+                        console.log(erro)
+                        res.status(400).json(erro)
+                    } else {
+                        res.status(200).json(novoPet)
+                    }
+                })
+            }
+        })
+    }
+}
+module.exports = new Pet()
